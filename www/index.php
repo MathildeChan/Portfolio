2fd70c888b1fd6197f9419f9d03e2790a3e6210b
Goré Mathilde <21803802@etu.unicaen.fr>
2020-03-07 18:35:12 +0100

    Commencement du portfolio

---
+++ www/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport">
+    <title>PortFolio - Mathilde Goré</title>
+    <link href="style/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+<header class="header">
+
+</header>
+    <div class="passport">
+    <div class="surround-passport">
+        <div class="inside-passport">
+            <div class="passport-img">
+            </div>
+            <div class="passport-info">
+            </div>
+        </div>
+    </div>
+    </div>
+    <footer class="footer">
+
+    </footer>
+</body>
+
+</html>
