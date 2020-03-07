27c2473ef01e3c1776d0860689b96989bb9e38ce
Goré Mathilde <21803802@etu.unicaen.fr>
2020-03-08 00:07:07 +0100

    Tentative de nuage

--- www/index.php
+++ www/index.php
@@ -6,25 +6,35 @@
     <meta name="viewport">
     <title>PortFolio - Mathilde Goré</title>
     <link href="style/main.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d2e9c2de17.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
-<header class="header">
-    <span class="header-name">&lsaquo; Mathilde Goré /&rsaquo;</span>
-    <img src="img/header-img.jpg" class="header-img">
-</header>
-    <div class="passport">
-    <div class="surround-passport">
-        <div class="inside-passport">
-            <div class="passport-img">
-            </div>
-            <div class="passport-info">
+    <header id="header">
+        <img class="header-img" src="img/Headerpic2.jpg" alt="Image représentant du code HTML">
+        <div class="header-text">
+            <h2>&lsaquo; Mathilde Goré /&rsaquo;</h2>
+        </div>
+    </header>
+
+    <div id="container">
+        <div class="passport">
+            <div class="surround-passport">
+                <div class="inside-passport">
+                    <div class="passport-img">
+                    </div>
+                    <div class="passport-info">
+                    </div>
+                </div>
             </div>
         </div>
+        <div id="cloud">
+            
+        </div>
     </div>
-    </div>
-    <footer class="footer">
+    <footer id="footer">
 
     </footer>
 </body>
